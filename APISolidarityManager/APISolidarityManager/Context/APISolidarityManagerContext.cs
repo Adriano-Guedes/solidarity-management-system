b3deaf96251b39af1547dc21a970c579fdd301de
Adriano-Guedes <adrianno.aguedes@gmail.com>
2026-03-18 01:05:40 -0300

    All classes models creation and dbcontext creation

---
+++ APISolidarityManager/APISolidarityManager/Context/APISolidarityManagerContext.cs
@@ -0,0 +1,23 @@
+﻿using APISolidarityManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APISolidarityManager.Context
+{
+    public class APISolidarityManagerContext : DbContext
+    {
+        public APISolidarityManagerContext(DbContextOptions<APISolidarityManagerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Beneficiary> Beneficiaries { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<DeliveryItem> DeliveryItems { get; set; }
+        public DbSet<Donation> Donations { get; set; }
+        public DbSet<DonationItem> DonationItems { get; set; }
+        public DbSet<Donor> Donors { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ APISolidarityManager/APISolidarityManager/Models/DonationItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Principal;
+
+namespace APISolidarityManager.Models
+{
+    public class DonationItem
+    {
+        public Guid DonationId { get; set; }
+        public Guid ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
